72a32110b874085b4bcd986fc0abfc3df89d6921
MaximButmanov <but-maxim@yandex.ru>
2026-01-30 12:36:21 +0300

    restore files

---
+++ src/main/java/com/max/inventory/InventoryReservationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.max.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryReservationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryReservationServiceApplication.class, args);
+	}
+}
